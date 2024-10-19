415d9a4ad237cecfc18ff9826e99d85932d79df4
saurav-ghimire <sauravghimire0123@gmail.com>
2024-10-18 21:29:26 -0400

    moved MongoDB credentials to .env file

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
-# MongoDB Atlas connection
-client = MongoClient("mongodb+srv://c0921819:$itaKrishna7@ecommerce-cluster.0xwr1.mongodb.net/?retryWrites=true&w=majority")
+# MongoDB Atlas connection with environment variables
+MONGODB_USERNAME = os.getenv('MONGODB_USERNAME')
+MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+
+client = MongoClient(f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}@ecommerce-cluster.0xwr1.mongodb.net/?retryWrites=true&w=majority")
 db = client['shop_db']
 products_collection = db['products']
 
